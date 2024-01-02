c178a6a885914d03a9170dfa0426ae8b557271c8
Ewelina <ewelina.petelicka01@gmail.com>
2024-01-02 14:39:20 +0100

    navigate feature to projects

--- src/components/core/header/Header.tsx
+++ src/components/core/header/Header.tsx
@@ -19,62 +19,67 @@ export function Header() {
         setAnchorElUser(null);
     };
 
-    function logOut(){
+    function logOut() {
         dispatch(logoutUser());
     }
 
     return (
         <AppBar position="static">
-                <Toolbar disableGutters sx={{ width:'100%', display: "flex", justifyContent: "space-between", pl:1, pr:2}}>
-                    <div style={{display: "flex", alignItems: "center", cursor:"pointer"}} onClick={() => navigate("/dashboard")}>
-                        <KeyboardCommandKeyIcon sx={{display: {xs: 'none', md: 'flex'}, mr: 1}} color="primary"
-                                                fontSize="large"/>
-                        <Typography
-                            variant="h5"
-                            noWrap
-                            sx={{
-                                mr: 2,
-                                display: {xs: 'none', md: 'flex'},
-                                fontFamily: 'monospace',
-                                fontWeight: 700,
-                                letterSpacing: '.3rem',
-                                textDecoration: 'none',
-                            }}
-                        >
-                            TaskForge
-                        </Typography>
-                    </div>
-                    <Box sx={{flexGrow: 0}}>
-                        <Tooltip title="Open settings">
-                            <IconButton onClick={handleOpenUserMenu} sx={{p: 0}}>
-                                <Avatar src={avatar}/>
-                            </IconButton>
-                        </Tooltip>
-                        <Menu
-                            sx={{mt: '45px'}}
-                            id="menu-appbar"
-                            anchorEl={anchorElUser}
-                            anchorOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            open={Boolean(anchorElUser)}
-                            onClose={handleCloseUserMenu}
-                        >
-                            <MenuItem onClick={handleCloseUserMenu}>
-                                <Typography textAlign="center">Profile</Typography>
-                            </MenuItem>
-                            <MenuItem onClick={()=>logOut()}>
-                                <Typography textAlign="center">Logout</Typography>
-                            </MenuItem>
-                        </Menu>
-                    </Box>
-                </Toolbar>
+            <Toolbar disableGutters
+                     sx={{width: '100%', display: "flex", justifyContent: "space-between", pl: 1, pr: 2}}>
+                <div style={{display: "flex", alignItems: "center", cursor: "pointer"}}
+                     onClick={() => navigate("/dashboard")}>
+                    <KeyboardCommandKeyIcon sx={{display: {xs: 'none', md: 'flex'}, mr: 1}} color="primary"
+                                            fontSize="large"/>
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        sx={{
+                            mr: 2,
+                            display: {xs: 'none', md: 'flex'},
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        TaskForge
+                    </Typography>
+                    <MenuItem onClick={() => navigate('/project-dashboard')}>
+                        <Typography textAlign="center">Products</Typography>
+                    </MenuItem>
+                </div>
+                <Box sx={{flexGrow: 0}}>
+                    <Tooltip title="Open settings">
+                        <IconButton onClick={handleOpenUserMenu} sx={{p: 0}}>
+                            <Avatar src={avatar}/>
+                        </IconButton>
+                    </Tooltip>
+                    <Menu
+                        sx={{mt: '45px'}}
+                        id="menu-appbar"
+                        anchorEl={anchorElUser}
+                        anchorOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                        }}
+                        keepMounted
+                        transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                        }}
+                        open={Boolean(anchorElUser)}
+                        onClose={handleCloseUserMenu}
+                    >
+                        <MenuItem onClick={handleCloseUserMenu}>
+                            <Typography textAlign="center">Profile</Typography>
+                        </MenuItem>
+                        <MenuItem onClick={() => logOut()}>
+                            <Typography textAlign="center">Logout</Typography>
+                        </MenuItem>
+                    </Menu>
+                </Box>
+            </Toolbar>
         </AppBar>
     )
 }
