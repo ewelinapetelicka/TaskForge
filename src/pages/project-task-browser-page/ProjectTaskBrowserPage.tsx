d1f10d3b43df569d335dd23155a99eca0d640132
Ewelina <ewelina.petelicka01@gmail.com>
2024-01-04 19:21:25 +0100

    task search feature

--- src/pages/project-task-browser-page/ProjectTaskBrowserPage.tsx
+++ src/pages/project-task-browser-page/ProjectTaskBrowserPage.tsx
@@ -1,12 +1,26 @@
-import {Input, InputAdornment, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
-import {Search} from "@mui/icons-material";
+import {
+    Icon,
+    Input,
+    InputAdornment,
+    Pagination,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TablePagination,
+    TableRow
+} from "@mui/material";
+import {Circle, Search} from "@mui/icons-material";
 import Paper from '@mui/material/Paper';
 import {useDispatch, useSelector} from "react-redux";
 import {selectTasks, setTasks} from "../../store/tasks/tasks.slice";
 import {useHttpClient} from "../../hooks/use-http-client/use-http-client";
 import {useParams} from "react-router-dom";
 import {Task} from "../../models/task/task";
-import {useEffect} from "react";
+import React, {useEffect, useState} from "react";
+import {TaskType} from "../../models/task/task-type/task-type";
+import {Project} from "../../models/project/project";
 
 
 export function ProjectTaskBrowserPage() {
@@ -14,51 +28,73 @@ export function ProjectTaskBrowserPage() {
     const http = useHttpClient();
     const params = useParams();
     const dispatch = useDispatch();
+    const [search, setSearch] = useState<string>("");
+    const [filteredTasks, setFilteredTasks] = useState<Task[]>([])
+
 
     useEffect(() => {
         getTasks();
-    }, []);
+        setFilteredTasks(tasks.filter((el)=> el.title.toLowerCase().includes(search.toLowerCase())));
+    }, [search,tasks]);
 
     function getTasks() {
         http.get("project/" + params.id + "/tasks")
             .then((tasks: Task[]) => dispatch(setTasks(tasks)))
     }
 
+    function colorType(task: string) {
+        if (task === TaskType.STORY) {
+            return <Circle color={"success"}></Circle>;
+        }
+        if (task === TaskType.BUG) {
+            return <Circle color={"error"}></Circle>;
+        }
+        if (task === TaskType.TECHNICAL_TASK) {
+            return <Circle color={"info"}></Circle>;
+        }
+    }
+
     return (
         <div>
-            <Input
-                id="input-with-icon-adornment"
-                startAdornment={
-                    <InputAdornment position="start">
-                        <Search/>
-                    </InputAdornment>
-                }
-            />
+            <div style={{width: '100%', display: 'flex', justifyContent: 'end', padding: '1rem', paddingRight: '5rem'}}>
+                <Input
+                    id="input-with-icon-adornment"
+                    onChange={(e) => setSearch(e.target.value)}
+                    startAdornment={
+                        <InputAdornment position="start">
+                            <Search/>
+                        </InputAdornment>
+                    }
+                />
+            </div>
             <TableContainer component={Paper}>
                 <Table sx={{minWidth: 650}} aria-label="simple table">
                     <TableHead>
                         <TableRow>
-                            <TableCell>TASK NAME</TableCell>
-                            <TableCell align="right">PRIORITY</TableCell>
-                            <TableCell align="right">STATUS</TableCell>
+                            <TableCell>TYPE</TableCell>
+                            <TableCell align="left">TASK NAME</TableCell>
+                            <TableCell align="center">PRIORITY</TableCell>
+                            <TableCell align="center">STATUS</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {tasks.map((task) => (
+                        {filteredTasks.map((task) => (
                             <TableRow
                                 key={task.id}
                                 sx={{'&:last-child td, &:last-child th': {border: 0}}}
                             >
-                                <TableCell component="th" scope="row">
+                                <TableCell sx={{width: '20px'}}>{colorType(task.type)}</TableCell>
+                                <TableCell align='left'>
                                     {task.title}
                                 </TableCell>
-                                <TableCell align="right">{task.priority}</TableCell>
-                                <TableCell align="right">{task.status}</TableCell>
+                                <TableCell align="center">{task.priority}</TableCell>
+                                <TableCell align="center">{task.status}</TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
                 </Table>
             </TableContainer>
+            <Pagination count={filteredTasks.length / 10} variant="outlined" color="secondary"/>
         </div>
     )
 }
