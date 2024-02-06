7253f2808feded5e8ece821541973e0479a864ce
Ewelina <ewelina.petelicka01@gmail.com>
2024-02-06 18:18:37 +0100

    adjust project icons adding

--- src/modules/projects/components/settings-general/SettingsGeneral.tsx
+++ src/modules/projects/components/settings-general/SettingsGeneral.tsx
@@ -9,6 +9,9 @@ import {selectUsers, selectUsersByIds} from "../../../../store/user/user.slice";
 import {useState} from "react";
 import {useHttpClient} from "../../../../hooks/use-http-client/use-http-client";
 import {enqueueSnackbar} from "notistack";
+import {Avatar} from "primereact/avatar";
+import {ListBox} from "primereact/listbox";
+import {projectsIconsOptions} from "../../const/projects-icons-options";
 
 export function SettingsGeneral() {
     const params = useParams<{ id: string }>();
@@ -18,6 +21,14 @@ export function SettingsGeneral() {
     const [editableProject, setEditableProject] = useState({...project});
     const http = useHttpClient();
     const dispatch = useDispatch();
+    const iconTemplate = (option: any) => {
+        return (
+            <div className="flex align-items-center gap-3">
+                <Avatar image={option.value} size={"large"}/>
+                <div>{option.label}</div>
+            </div>
+        );
+    };
 
     function editProject() {
         http.patch("projects/" + editableProject.id, {...editableProject}).then(() => {
@@ -30,13 +41,19 @@ export function SettingsGeneral() {
         <div className="flex flex-column gap-3">
             <InputText value={editableProject.title}
                        onChange={(e) => setEditableProject({...editableProject, title: e.target.value})}></InputText>
-            <InputText value={editableProject.icon}
-                       onChange={(e) => setEditableProject({...editableProject, icon: e.target.value})}></InputText>
             <InputTextarea value={editableProject.description} className={"h-14rem"}
                            onChange={(e) => setEditableProject({
                                ...editableProject,
                                description: e.target.value
                            })}></InputTextarea>
+            <div className={"flex gap-3 align-items-center"}>
+                <Avatar image={editableProject.icon} size={"xlarge"}/>
+                <ListBox value={projectsIconsOptions}
+                         onChange={(e) => setEditableProject({...editableProject, icon: e.target.value})}
+                         optionLabel={"name"}
+                         itemTemplate={iconTemplate} options={projectsIconsOptions} className="w-full "
+                         listStyle={{maxHeight: '150px', minWidth: '400px'}}></ListBox>
+            </div>
             <MultiSelect value={assignee} options={users} optionLabel="name" display="chip"
                          onChange={(e) => setEditableProject({...editableProject, userIds: e.target.value})}/>
             <div className={"w-12 flex justify-content-center "}>
---
+++ src/modules/projects/const/projects-icons-options.ts
@@ -0,0 +1,23 @@
+import {Option} from "../../../models/option/option";
+import {ProjectsIcons} from "../models/project/projects-icons/projects-icons";
+
+export const projectsIconsOptions: Option<ProjectsIcons>[] = [
+    {label: "Camera", value: ProjectsIcons.CAMERA},
+    {label: "Sun", value: ProjectsIcons.SUN},
+    {label: "Flower", value: ProjectsIcons.FLOWER},
+    {label: "Calculator", value: ProjectsIcons.CALCULATOR},
+    {label: "Wave", value: ProjectsIcons.WAVE},
+    {label: "Smiley", value: ProjectsIcons.SMILEY},
+    {label: "Bell", value: ProjectsIcons.BELL},
+    {label: "Gift", value: ProjectsIcons.GIFT},
+    {label: "Key", value: ProjectsIcons.KEY},
+    {label: "Bike", value: ProjectsIcons.BIKE},
+    {label: "Star", value: ProjectsIcons.STAR},
+    {label: "Lightning", value: ProjectsIcons.LIGHTNING},
+    {label: "Compass", value: ProjectsIcons.COMPASS},
+    {label: "Store", value: ProjectsIcons.STORE},
+    {label: "Sign", value: ProjectsIcons.SIGN},
+    {label: "Hourglass", value: ProjectsIcons.HOURGLASS},
+    {label: "Diamond", value: ProjectsIcons.DIAMOND}
+
+]
